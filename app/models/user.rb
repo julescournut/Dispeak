eef71a127b8e5cd728d14f1967060085009482bf
the-gameur-63 <the-gameur-63@hotmail.fr>
2017-05-18 09:00:29 -0400

    merge jules moi authentification db hash ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,36 @@
-class User < ApplicationRecord
+require 'digest'
+class User < ActiveRecord::Base
+  before_save :encrypt_password
+  validates :password, :presence     => true,
+                       :confirmation => true,
+                       :length       => { :within => 6..40 }
+
+  def has_password?(password_soumis)
+    encrypted_password == encrypt(password_soumis)
+  end
+  
+  def self.authenticate(name, submitted_password)
+    user = find_by_name(name)
+    return nil  if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+
+  private
+
+    def encrypt_password
+      self.salt = make_salt if new_record?
+      self.encrypted_password = encrypt(password)
+    end
+
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
 
-  resources :users, only: [:index, :show, :create]
 
+  resources :users, only: [:index, :show, :create]
+ 	resources :sessions, :only => [:new, :create, :destroy]
+	
+  get 'signin' => 'sessions#new'
+  get '/signout' => 'sessions#destroy'
+	get '/signup' => 'users#new' # TODO JULES : mettre ton vrai lien d'inscription #!/login
+	
 	root 'welcome#index'	
 	get '/api' => 'application#api', defaults: { format: :json }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
